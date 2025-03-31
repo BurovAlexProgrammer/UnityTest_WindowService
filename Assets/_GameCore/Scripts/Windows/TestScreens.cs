e113fd7c1f16a07bcce070bd857662a2c625899b
Aleksey Burov <burovalexprogrammer@gmail.com>
2025-03-31 14:24:19 +0200

    Screen1 test

---
+++ Assets/_GameCore/Scripts/Windows/TestScreens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AppCoreModule.Scripts.Services;
+using AppCoreModule.Scripts.UI.Screens;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _GameCore.Scripts.Windows
+{
+    public class TestScreens : MonoBehaviour
+    {
+        [SerializeField] private BaseScreen _screen1;
+        [SerializeField] private Canvas _screenCanvas;
+        
+        private void Awake()
+        {
+            var windowService = new ScreenService();
+            windowService.Init(_screenCanvas);
+            windowService.OpenWindow(_screen1);
+        }
+    }
+}
